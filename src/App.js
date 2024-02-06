402550d7c3e327a10cf4acd42adcd246676bba8e
trongminhle <leminhtrong25012000@gmail.com>
2024-02-06 17:15:10 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventDetailPage, {
+   loader as eventsDetailLoader,
+   action as deleteEventAction,
+} from "./pages/EventDetail";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+         {
+            index: true,
+            element: <HomePage />,
+         },
+
+         {
+            path: "events",
+            element: <EventsRootLayout />,
+            children: [
+               {
+                  index: true,
+                  element: <EventsPage />,
+                  loader: eventsLoader,
+               },
+
+               {
+                  path: ":eventId",
+                  id: "event-detail",
+                  loader: eventsDetailLoader,
+                  children: [
+                     {
+                        index: true,
+                        element: <EventDetailPage />,
+                        action: deleteEventAction,
+                     },
+                     {
+                        path: "edit",
+                        element: <EditEventPage />,
+                     },
+                  ],
+               },
+               {
+                  path: "new",
+                  element: <NewEventPage />,
+                  action: newEventAction,
+               },
+            ],
+         },
+      ],
+   },
+]);
+
+function App() {
+   return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
---
+++ src/pages/Events.js
@@ -0,0 +1,32 @@
+import { useLoaderData, json } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+   const data = useLoaderData();
+   const events = data.events;
+
+   return (
+      <>
+         <EventsList events={events} />
+      </>
+   );
+}
+
+export default EventsPage;
+
+export const loader = async () => {
+   const response = await fetch("http://localhost:8080/events");
+
+   if (!response.ok) {
+      // throw new Response(JSON.stringify({ message: "Could not fetch events." }), { status: 500 });
+      throw json(
+         { message: "Could not fetch events." },
+         {
+            status: 500,
+         }
+      );
+   } else {
+      return response;
+   }
+};
